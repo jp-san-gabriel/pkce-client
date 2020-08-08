75bcd88bcccf084b9c1c4c456cb843de55ddc517
JEAN PAUL T. SAN GABRIEL <jp.san.gabriel01@gmail.com>
2020-08-08 17:10:29 +0800

    Customize app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Link to="/login">Login</Link> |&nbsp;
+        <Link to="/main">Main</Link> |&nbsp;
+        <Link to="/about">About</Link> |&nbsp;
+      </div>
+      <Switch>
+        <Route path="/login">
+          Login
+        </Route>
+        <Route path="/main">
+          Main
+        </Route>
+        <Route path="/about">
+          About
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
